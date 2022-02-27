9a651db356c7e02828171dd79a788a09f33a9904
RachaelColaco <65530256+RachaelColaco@users.noreply.github.com>
2022-02-27 15:33:27 +1300

    bug fix: activate save process UI animation

--- Assets/Scripts/rachael/BookEntry.cs
+++ Assets/Scripts/rachael/BookEntry.cs
@@ -30,7 +30,7 @@ namespace rachael
             {
                 // saves the player data into the system
                 tempPlayer.GetComponent<PlayerSave>().SavePlayer();
-                //_player.SaveProcess.SetTrigger("Save");
+                _player.SaveProcess.SetTrigger("Save");
                 //Debug.Log(eraChanging.Rooms[eraChanging.Index].name);
                 //
                 //DefineRoom(m_eraChanging.m_rooms[m_eraChanging.index].name);
