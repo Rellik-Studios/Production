cd1f93bcb977a3fa8c3251725b372b32c2c8910e
RachaelColaco <65530256+RachaelColaco@users.noreply.github.com>
2022-03-02 14:35:52 +1300

    One shot animation and two animations state scripts

---
+++ Assets/Scripts/rachael/AnimInteract/TwoAnimInteract.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using Himanshu;
+using UnityEngine;
+namespace rachael
+{
+    public class TwoAnimInteract : MonoBehaviour, IInteract
+    {
+        // Start is called before the first frame update
+        private Animator m_objectAnim;
+
+        //private RaycastingTesting m_raycastingTesting;
+
+        private bool m_objectOpen = false;
+
+        public bool isObjectOpen => m_objectOpen;
+
+        public bool objectOpen
+        {
+            get => m_objectOpen;
+            set
+            {
+                m_objectOpen = value;
+                m_objectAnim.SetBool("IsOpening", value);
+            }
+        }
+
+
+        private void Awake()
+        {
+            m_objectAnim = GetComponent<Animator>();
+            //m_raycastingTesting = FindObjectOfType<RaycastingTesting>();
+        }
+
+        public void PlayAnimation()
+        {
+            if (!objectOpen)
+            {
+                OpenObject();
+            }
+            else
+            {
+                CloseObject();
+            }
+        }
+
+        public void CloseObject()
+        {
+            objectOpen = false;
+        }
+
+        public void OpenObject()
+        {
+            objectOpen = true;
+        }
+
+        public void Execute(PlayerInteract _player)
+        {
+            PlayAnimation();
+        }
+
+        public void CanExecute(Raycast _raycast)
+        {
+            if (!_raycast.m_doOnce)
+            {
+                _raycast.CrosshairChange(true);
+            }
+            //temp until UI for the opening/closing door
+            _raycast.m_indication.enabled = false;
+            //--------------------------
+            _raycast.m_doOnce = true;
+        }
+
+
+    }
+}
