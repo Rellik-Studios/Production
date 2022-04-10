bfd489e4a14692d67a64c287d4a440e1f50210c9
himchawla <himchawla43@gmail.com>
2022-04-11 08:43:43 +1200

    ResetTransform lesgoo

--- Assets/Scripts/Himanshu/SmartObjective/ResetTransform.cs
+++ Assets/Scripts/Himanshu/SmartObjective/ResetTransform.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using rachael.FavorSystem;
 using UnityEngine;
@@ -7,14 +8,31 @@ namespace Himanshu.SmartObjective
     {
         [SerializeField] private List<Animator> m_animators;
         private static ResetTransform instance;
+        private bool m_hasReset;
+        [SerializeField] private Objective m_objective;
+        private FavorSystem m_favorSystem;
         private void Start()
         {
+            m_favorSystem = FindObjectOfType<FavorSystem>();
             instance = this;
         }
         
         public static bool ResetT()
         {
-            instance.m_animators.ForEach(animator => animator.SetTrigger("ResetT"));
+            IEnumerator ResetCoroutine()
+            {
+
+                instance.m_favorSystem.CloseCommandPrompt();
+                yield return new WaitForSeconds(0.3f);
+                instance.m_animators.ForEach(animator => animator.SetTrigger("ResetT"));
+                instance.m_hasReset = true;
+                instance.m_objective.Execute(FindObjectOfType<PlayerInteract>());
+            }
+            if (gameManager.Instance.m_currentRoom != "Morden Bedroom" || instance.m_hasReset)
+                return false;
+            instance.StartCoroutine(ResetCoroutine());
+
+
             return true;
         }
         
