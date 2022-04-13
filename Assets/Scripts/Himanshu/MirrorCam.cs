56ed0fa43e07920ba6b3c586a8f703f5c7b4fc01
himchawla <himchawla43@gmail.com>
2022-04-13 20:59:01 +1200

    magic mirror on the wall

---
+++ Assets/Scripts/Himanshu/MirrorCam.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+namespace Himanshu
+{
+    public class MirrorCam : MonoBehaviour
+    {
+        [SerializeField] private GameObject m_playerCam;
+
+        public Vector3 m_defPlayerPos;
+        public Quaternion m_defPlayerRot;
+        private static MirrorCam m_instance;
+        private static bool m_beginTracking;
+
+        private Vector3 m_defPos;
+        private Quaternion m_defRot;
+
+        public static bool beginTracking
+        {
+            get => m_beginTracking;
+            set
+            {
+                m_beginTracking = value;
+                if (value)
+                {
+                    m_instance.m_defPlayerPos = m_instance.m_playerCam.transform.position;
+                    m_instance.m_defPlayerRot = m_instance.m_playerCam.transform.rotation;
+                }
+            }
+        }
+
+        private void Start()
+        {
+            m_instance = this;
+            m_defPos = transform.position;
+            m_defRot = transform.rotation;
+        }
+
+        private void Update()
+        {
+            if(m_beginTracking)
+            {
+                transform.position = m_defPos + (m_playerCam.transform.position - m_defPlayerPos);
+                transform.rotation = m_playerCam.transform.rotation;
+            }
+        }
+    }
+}
