b32f2ef8c2b0946851cc1ece1209cb8580d6faeb
RachaelColaco <65530256+RachaelColaco@users.noreply.github.com>
2022-05-09 21:23:23 +1200

    Hand script for clock hands

--- Assets/Scripts/Himanshu/Hand.cs
+++ Assets/Scripts/Himanshu/Hand.cs
@@ -4,25 +4,27 @@ namespace Himanshu
 {
     public class Hand : MonoBehaviour, IInteract
     {
-
+        public bool HandOpen;
         private Animator m_animator;
 
         [SerializeField] private Objective m_objective;
         private void Start()
         {
             m_animator = GetComponent<Animator>();
-            m_objective.m_locked = !aIsHandOpen;
+            aIsHandOpen = HandOpen;
+            m_objective.m_locked = aIsHandOpen;
+            
         }
 
         private bool aIsHandOpen {
-            get => m_animator.GetBool("IsOpen");
-            set => m_animator.SetBool("IsOpen", value);
+            get => m_animator.GetBool("IsOpening");
+            set => m_animator.SetBool("IsOpening", value);
         }
         
         public void Execute(PlayerInteract _player)
         {
             aIsHandOpen = !aIsHandOpen;
-            m_objective.m_locked = !aIsHandOpen;
+            m_objective.m_locked = aIsHandOpen;
         }
         public void CanExecute(Raycast _raycast)
         {
